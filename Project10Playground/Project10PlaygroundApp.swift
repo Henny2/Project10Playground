41687461e9f7e3a088cf67005cc424177a20529b
Henny2 <henrieke.baunack@web.de>
2023-12-07 18:15:42 -0800

    Initial Commit

---
+++ Project10Playground/Project10PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project10PlaygroundApp.swift
+//  Project10Playground
+//
+//  Created by Henrieke Baunack on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Project10PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
